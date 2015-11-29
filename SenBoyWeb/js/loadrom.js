b98d371a414855b1d6154af3d8d0de1a1b7f17f1
Senryoku <maretverdant@gmail.com>
2015-11-29 14:35:11 +0100

    Obviously, forgot a file :D

--- SenBoyWeb/js/loadrom.js
+++ SenBoyWeb/js/loadrom.js
@@ -1,22 +1,46 @@
-function readSingleFile(e) {
-  var file = e.target.files[0];
-  if (!file) {
-    return;
-  }
-  var reader = new FileReader();
-  reader.onload = function(e) {
-    var contents = e.target.result;
-	var buf = Module._malloc(contents.byteLength);
-	var dataHeap = new Uint8Array(Module.HEAPU8.buffer, buf, contents.byteLength);
-	dataHeap.set(new Uint8Array(contents));
-	Module.ccall('load_rom', 'number', ['number', 'number'], [dataHeap.byteOffset, contents.byteLength]);
-	Module._free(buf);
-  };
-  reader.readAsArrayBuffer(file);
+function read_server_side_file(file_path) {
+	var xhr = new XMLHttpRequest(); 
+	xhr.open("GET", file_path);
+	xhr.responseType = "blob";
+	xhr.onload = function()
+	{
+		var blob = xhr.response;
+	
+		var reader = new FileReader();
+		reader.onload = function(e) {
+		var contents = e.target.result;
+		var buf = Module._malloc(contents.byteLength);
+		var dataHeap = new Uint8Array(Module.HEAPU8.buffer, buf, contents.byteLength);
+		dataHeap.set(new Uint8Array(contents));
+		Module.ccall('load_rom', 'number', ['number', 'number'], [dataHeap.byteOffset, contents.byteLength]);
+		Module._free(buf);
+		};
+		reader.readAsArrayBuffer(blob);
+	}
+	xhr.send();
+}
+
+function read_local_file(e) {
+	var file = e.target.files[0];
+	if (!file) {
+		return;
+	}
+	var reader = new FileReader();
+	reader.onload = function(e) {
+		var contents = e.target.result;
+		var buf = Module._malloc(contents.byteLength);
+		var dataHeap = new Uint8Array(Module.HEAPU8.buffer, buf, contents.byteLength);
+		dataHeap.set(new Uint8Array(contents));
+		Module.ccall('load_rom', 'number', ['number', 'number'], [dataHeap.byteOffset, contents.byteLength]);
+		Module._free(buf);
+	};
+	reader.readAsArrayBuffer(file);
+
+	read_server_side_file("pocket.gb");
 }
 
 document.getElementById('file-input')
-  .addEventListener('change', readSingleFile, false);
+  .addEventListener('change', read_local_file, false);
  
 function file_load(r)
 {
@@ -25,3 +49,14 @@ function file_load(r)
 
 document.getElementById('load-rom')
   .addEventListener('click', file_load, false);
+  
+function local_server_rom(r)
+{
+	var ddl = document.getElementById('dropdown-roms');
+	var path = ddl.options[ddl.selectedIndex].value;
+	read_server_side_file(path);
+}
+
+document.getElementById('server-roms')
+  .addEventListener('click', local_server_rom, false);
+  
