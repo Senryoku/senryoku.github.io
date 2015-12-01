d7280eb3002161d74022680b9ed74d6da6015a1b
Senryoku <maretverdant@gmail.com>
2015-12-01 09:22:55 +0100

    Update SenBoyWeb

--- SenBoyWeb/js/soundcontrol.js
+++ SenBoyWeb/js/soundcontrol.js
@@ -1,5 +1,25 @@
+var sound = false;
+var waiting_sound = false;
+
+function stop_sound() {
+	if(sound)
+	{
+		toggle_sound();
+		waiting_sound = true;
+	}
+}
+
+function check_sound() {
+	if(waiting_sound && !sound)
+	{
+		toggle_sound();
+		waiting_sound = false;
+	}
+}
+
 function toggle_sound() {
 	Module.ccall('toggle_sound', 'number', [], []);
+	sound = !sound;
 }
 	
 document.getElementById('sound-control')
